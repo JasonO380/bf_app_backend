231acd617afb90d4f3d2427a2438eb971b09e5fd
Jason Ollada <JMOllada@gmail.com>
2023-05-16 13:38:11 -0500

    Added port for render

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ mongoose
         console.log("Error connecting to MongoDB", err);
     });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server started on port 5000");
 });
